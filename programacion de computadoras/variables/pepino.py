2742ab9f1b5bd0a8de76be069658c86d6b3cd123
keve3443 <tuemail@example.com>
2025-05-04 16:03:59 -0500

    tarea

---
+++ programacion de computadoras/variables/pepino.py
@@ -0,0 +1,16 @@
+print("esteves mi primer cuso de programacion")
+
+#hola a todos 
+float=1.100
+
+int=1234,43443
+print(int)
+float=3
+print(float)
+
+booleano=34+34
+print(booleano)
+nombre="kevin"
+apellido="garces"
+nombre_completo= "hola"+" "+ nombre   +" "+ apellido
+print(nombre_completo)
